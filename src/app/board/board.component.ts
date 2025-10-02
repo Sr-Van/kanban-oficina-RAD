e83e7e02758bc17015ff9db1b42062d99b41e7c7
Vanderson Erlan <vandersonerlan@Vandersons-MacBook-Pro.local>
2025-10-02 16:12:11 -0300

    feat: implement cell component into board
    This commit does separeted things on the same file:
    - Import cell component
    - create mocked array of orders
    - filter the array of orders by status, creating 3 arrays (to do, in progress, done)
    - iterate each array in the proper column and pass the input property for each item to cell component
    - adapt the styles

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,46 @@
+import { Order } from './../shared/order.interface';
 import { Component } from '@angular/core';
+import { CellComponent } from '../shared/cell/cell.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CellComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+  styleUrl: './board.component.css',
 })
 export class BoardComponent {
+  private mockData: Order[] = [
+    {
+      id: 1,
+      title: 'Order 1',
+      description: 'Description 1',
+      employee: 'John Doe',
+      status: 'To Do',
+    },
+    {
+      id: 2,
+      title: 'Order 2',
+      description: 'Description 2',
+      employee: 'Jane Doe',
+      status: 'In Progress',
+    },
+    {
+      id: 3,
+      title: 'Order 3',
+      description: 'Description 3',
+      employee: 'John Doe',
+      status: 'Done',
+    },
+  ];
 
+  public toDoItems: Order[] = this.mockData.filter(
+    (item) => item.status === 'To Do'
+  );
+  public inProgressItems: Order[] = this.mockData.filter(
+    (item) => item.status === 'In Progress'
+  );
+  public doneItems: Order[] = this.mockData.filter(
+    (item) => item.status === 'Done'
+  );
 }
