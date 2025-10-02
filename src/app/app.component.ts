cddc49600c95f770e24b1caabf6eea39eca1d834
Vanderson Erlan <vandersonerlan@Vandersons-MacBook-Pro.local>
2025-10-02 15:37:42 -0300

    feat: kanban board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'oficina-RAD';
